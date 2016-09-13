cc6bfad5186fd4e2476f068042cd51791e4837de
Wang Zhaosong <jack19920125@163.com>
2016-09-13 22:21:21 +0800

    automatic type conversion

---
+++ AutomaticTypeConversion.cpp
@@ -0,0 +1,17 @@
+// Type conversion constructor
+class One {
+    public:
+        One() {}
+};
+
+class Two {
+    public:
+        Two(const One&) {}
+};
+
+void f(Two) {}
+
+int main() {
+    One one;
+    f(one); // Wants a Two, has a One
+}
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Some example when learning C++
 *Instructment4.cpp* - Pure virtual function
 
 *ObjectSlicing.cpp* - Object slicing, converting derived class to base class
+
+*AutomaticTypeConversion.cpp* - Automatic type conversion
